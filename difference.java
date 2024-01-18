b91eea539751b822944dd2985f5b616023fa3d7d
Adiba <Adibashaikhani37@gmail.com>
2024-01-18 10:06:01 +0530

    Commit Complete

---
+++ difference.java
@@ -0,0 +1,10 @@
+public class difference {
+        public static void main(String[] args) {
+            int a=15;
+            int b=16;
+            int c=20;
+            System.out.println(a+b+c);
+        }
+}
+    
+
